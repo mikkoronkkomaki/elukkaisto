fc5c14a8a6e59289da60f542135c2e9dcc86b81a
Mikko Rönkkömäki <mikko.ronkkomaki@solita.fi>
2022-08-11 15:17:29 +0300

    Chonk

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# testikanta
+# Elukkaisto
 
 FIXME
 
--- test/elukkaisto/kyselyt/elaimet_test.clj
+++ test/elukkaisto/kyselyt/elaimet_test.clj
@@ -6,17 +6,17 @@
 (use-fixtures :each fikstuuri/tietokanta-fikstuuri)
 
 (deftest hae-kaikki-elaimet
-  (println "--->>> kaikki eläimet" (elaimet/hae-kaikki-elaimet @fikstuuri/db)))
+  (def kaikki-elamiet-haussa (elaimet/hae-kaikki-elaimet @fikstuuri/db)))
 
 (deftest lisaa-elain
-  (println "--->>> kaikki eläimet" (elaimet/hae-kaikki-elaimet @fikstuuri/db))
+  (def ennen-lisaysta (elaimet/hae-kaikki-elaimet @fikstuuri/db))
   (elaimet/lisaa-uusi-elain @fikstuuri/db {:nimi "Vompatti" :latinankielinen-nimi "Vombatidae"})
-  (println "--->>> kaikki eläimet" (elaimet/hae-kaikki-elaimet @fikstuuri/db)))
+  (def lisayksen-jalkeen (elaimet/hae-kaikki-elaimet @fikstuuri/db)))
 
 (deftest paivita-elain
-  (println "--->>> kaikki eläimet" (elaimet/hae-kaikki-elaimet @fikstuuri/db))
+  (def ennen-paivitysta (elaimet/hae-kaikki-elaimet @fikstuuri/db))
   (let [paivitettava-id (:id (first (filter #(= "Kettu" (:nimi %)) (elaimet/hae-kaikki-elaimet @fikstuuri/db))))]
     (elaimet/paivita-elain @fikstuuri/db {:id paivitettava-id
                                            :nimi "Viiksisiippa"
                                            :latinankielinen-nimi "Myotis mystacinus"}))
-  (println "--->>> kaikki eläimet" (elaimet/hae-kaikki-elaimet @fikstuuri/db)))
+  (def paivityksen-jalkeen (elaimet/hae-kaikki-elaimet @fikstuuri/db)))
